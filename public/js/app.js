f6baaed9e191f2ac233d2c58ba4e245b085a562d
Chris Whong <chris.whong@socrata.com>
2015-03-21 17:52:30 -0400

    Search Enhancements

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,37 @@
 var myApp = angular.module('mainApp', ['angular.filter']);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
+    //set the default sort
+    $scope.sortBy = '+title';
+    $scope.searchString = '';  
+
+    //get data.json
     $http.get('/data.json').
       success(function(data, status, headers, config) {
         $scope.datasets = data;
       });
 
-  
+    // $scope.updateQuery = function() {
+    //   console.log('updateQuery Ran!');
+    //   $scope.query = {
+    //     title: $scope.searchString,
+    //     description: $scope.searchString
+    //   }
+
+    //   console.log($scope.query);
+    // }
+
+    $scope.mainFilter = function(dataset) {
+      console.log('mainfilter');
+      
+      return dataset.title.match(new RegExp($scope.searchString,'i')) ||
+        dataset.description.match(new RegExp($scope.searchString,'i'));
+      
+    }
+
 }]);
 
+//generates an array of unique tags for display in the sidebar
 myApp.filter('uniqueTags', function() {
     return function(datasets) {
         var tags = {};
@@ -22,7 +45,25 @@ myApp.filter('uniqueTags', function() {
             uniqueTags.push(key);
         }
 
-        console.log(uniqueTags);
+        // console.log(uniqueTags);
         return uniqueTags;
     }
+});
+
+
+/*
+This directive allows us to pass a function in on an enter key to do what we want.
+ */
+myApp.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
 });

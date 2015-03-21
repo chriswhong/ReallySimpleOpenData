480657f7d0aa1d2c9fde4b9f93a51e2881704223
Chris Whong <chris.whong@socrata.com>
2015-03-21 15:57:02 -0400

    Setting up for Heroku

--- mongoconnect.js
+++ mongoconnect.js
@@ -1,12 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
-var Server = require('mongodb').Server;
+
+// var Server = require('mongodb').Server;
 
 
-var mongoClient = new MongoClient(new Server('localhost', 27017));
+
+
+//var mongoClient = new MongoClient(new Server('localhost', 27017));
+
 var data;
 
-mongoClient.open(function(err, mongoClient) {
-  var db = mongoClient.db('rsod');
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/rsod';
+
+MongoClient.connect(mongoUri, function(err, db) {
+  //var db = mongoClient.db('rsod');
   data = db.collection('baltimore');
 });
 
